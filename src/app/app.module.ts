a6a9f39a4accb2ba87b0492b18e0036d26dc9b24
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2023-10-19 15:07:29 +0300

    feat: implement functionality of currency exchange component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ConversionComponent } from './components/conversion/conversion.component';
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, ConversionComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/conversion/conversion.component.ts
+++ src/app/components/conversion/conversion.component.ts
@@ -3,9 +3,35 @@ import { IExchangeRates } from "src/app/models/exchange-rates";
 
 @Component({
   selector: 'app-conversion',
-  templateUrl: './conversion.component.html'
+  templateUrl: './conversion.component.html',
 })
-
 export class ConversionComponent {
+  currency1: string = 'USD';
+  amount1: number = 0;
+  currency2: string = 'UAH';
+  amount2: number = 0;
+
   @Input() exchangeRates: IExchangeRates;
+
+  convert1to2(): void {
+    if (this.exchangeRates) {
+      const exchangeRate1 = this.exchangeRates[this.currency1].value;
+      const exchangeRate2 = this.exchangeRates[this.currency2].value;
+
+      this.amount2 = this.amount1 * (1 / exchangeRate1) * exchangeRate2;
+
+      this.amount2 = parseFloat(this.amount2.toFixed(2));
+    }
+  }
+
+  convert2to1(): void {
+    if (this.exchangeRates) {
+      const exchangeRate1 = this.exchangeRates[this.currency1].value;
+      const exchangeRate2 = this.exchangeRates[this.currency2].value;
+
+      this.amount1 = this.amount2 * (1 / exchangeRate2) * exchangeRate1;
+
+      this.amount1 = parseFloat(this.amount1.toFixed(2));
+    }
+  }
 }
