21d8deb30e04c751257eedf80b13c1f9f8262150
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2023-10-19 16:03:22 +0300

    refactor: move conversion logic to a separate function

--- src/app/components/conversion/conversion.component.ts
+++ src/app/components/conversion/conversion.component.ts
@@ -7,31 +7,38 @@ import { IExchangeRates } from "src/app/models/exchange-rates";
 })
 export class ConversionComponent {
   currency1: string = 'USD';
-  amount1: number = 0;
+  amount1: number = 1;
   currency2: string = 'UAH';
   amount2: number = 0;
 
   @Input() exchangeRates: IExchangeRates;
 
+  ngOnChanges(): void {
+    this.convert1to2();
+  }
+
   convert1to2(): void {
     if (this.exchangeRates) {
-      const exchangeRate1 = this.exchangeRates[this.currency1].value;
-      const exchangeRate2 = this.exchangeRates[this.currency2].value;
-
-      this.amount2 = this.amount1 * (1 / exchangeRate1) * exchangeRate2;
-
-      this.amount2 = parseFloat(this.amount2.toFixed(2));
+      this.amount2 = this.convert(this.currency1, this.currency2, this.amount1);
     }
   }
 
   convert2to1(): void {
     if (this.exchangeRates) {
-      const exchangeRate1 = this.exchangeRates[this.currency1].value;
-      const exchangeRate2 = this.exchangeRates[this.currency2].value;
-
-      this.amount1 = this.amount2 * (1 / exchangeRate2) * exchangeRate1;
-
-      this.amount1 = parseFloat(this.amount1.toFixed(2));
+      this.amount1 = this.convert(this.currency2, this.currency1, this.amount2);
     }
+  }
+
+  convert(
+    currency1: string,
+    currency2: string,
+    amount1: number,
+  ): number {
+    const exchangeRate1 = this.exchangeRates[currency1].value;
+    const exchangeRate2 = this.exchangeRates[currency2].value;
+
+    const newAmount = amount1 * (1 / exchangeRate1) * exchangeRate2;
+
+    return parseFloat(newAmount.toFixed(2));
   }
 }
